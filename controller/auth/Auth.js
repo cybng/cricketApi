f51e3d50331f633a1a4fa3bda765fa0bb8262c72
rahul <cybngr@gmail.com>
2021-10-24 11:51:31 -0700

    updated key

--- controller/auth/Auth.js
+++ controller/auth/Auth.js
@@ -1,8 +1,31 @@
 const User = require("../../modal/auth/userModal");
+const {msg} = require("../../helper/Messages");
 exports.login=(req,res)=>{
     return res.status(200).json({data:"login Successfull"});
 }
 
 exports.reg=(req,res)=>{
-      return res.status(200).json(req.body);
+      User.findOne({username:req.body.username})
+          .exec((err,data)=>{
+            if(data){
+                return returnStatus(201,0,res);
+            }
+            const {fname,lname,username,password} = req.body;
+            const userData = new User({
+                fname,lname,username,password
+            });
+            userData.save((err,data)=>{
+                if(err){
+                    return returnStatus(201,3,res);
+                }
+                if(data){
+                   return returnStatus(200,1,res);
+                }
+
+            });
+          });
+}
+
+const returnStatus = (status,msgKey,res)=>{
+     res.status(status).json({msg:`${msg[msgKey]}`});
 }
---
+++ helper/Messages.js
@@ -0,0 +1,12 @@
+exports.msg = [
+       "Allready Exist",
+       "Success",
+       "Not Found",
+       "Something Went Wrong",
+       "Server Error",
+       "Loading",
+       "Developement Mode",
+       "Surprize..",
+       "Offer",
+       "Discount",
+       ]
