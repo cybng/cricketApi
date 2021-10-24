85ef23640aad783a5686584cba0933b047ad91e4
rahul <cybngr@gmail.com>
2021-10-24 15:32:43 -0700

    update V

--- router/users.js
+++ router/users.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userModal=require("../modal/auth/userModal");
+const {validReg,isValid} = require("../validation/Auth")
 const {login,reg} = require("../controller/auth/Auth");
 
  router.post("/login",login);
- router.post("/reg",reg)
+ router.post("/reg",validReg,isValid,reg);
 
 
 module.exports = router;
---
+++ validation/Auth.js
@@ -0,0 +1,17 @@
+const {check,validationResult} = require("express-validator");
+
+exports.validReg=[
+        check("fname").notEmpty().withMessage("Firstname is Required"),
+        check("lname").notEmpty().withMessage("Lastname is Required"),
+        check("username").notEmpty().withMessage("Username is Required"),
+        check("password").isLength({min:6}).withMessage("Password Min 6 Digit is Required"),
+];
+
+
+exports.isValid = (req,res,next)=>{
+    const errors = validationResult(req);
+    if(errors.array().length>0){
+    	return res.status(201).json({error:errors.array()[0].msg});
+    }
+    next();
+}
