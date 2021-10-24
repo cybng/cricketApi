fc8030f17621f7ea51cb7fbc9ddfa7764493ebe7
rahul <cybngr@gmail.com>
2021-10-24 11:04:46 -0700

    updated c

--- controller/auth/Auth.js
+++ controller/auth/Auth.js
@@ -1,4 +1,8 @@
-
+const User = require("../../modal/auth/userModal");
 exports.login=(req,res)=>{
     return res.status(200).json({data:"login Successfull"});
+}
+
+exports.reg=(req,res)=>{
+      return res.status(200).json(req.body);
 }
--- router/users.js
+++ router/users.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userModal=require("../modal/auth/userModal");
-const {login} = require("../controller/auth/Auth");
+const {login,reg} = require("../controller/auth/Auth");
 
  router.post("/login",login);
+ router.post("/reg",reg)
 
 
 module.exports = router;
